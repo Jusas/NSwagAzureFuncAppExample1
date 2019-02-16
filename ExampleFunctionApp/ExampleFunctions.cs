179f34d5b1594081e1406f0aae9ac0de70dbb987
Jussi Saarivirta <jusasi@gmail.com>
2019-02-16 21:05:15 +0200

    Example project

---
+++ ExampleFunctionApp/ExampleFunctions.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using NSwag.Annotations;
+using NSwag.Annotations.AzureFunctionsV2;
+using NSwag.SwaggerGeneration.AzureFunctionsV2;
+
+namespace ExampleFunctionApp
+{
+    public static class ExampleFunctions
+    {
+        public class MyGreeting
+        {
+            public string Message { get; set; }
+        }
+
+        [SwaggerResponse(200, typeof(MyGreeting))]
+        [SwaggerQueryParameter("name", true, typeof(string), "The caller's name")]
+        [FunctionName("hello")]
+        public static async Task<IActionResult> Hello(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello")]
+            HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult(new MyGreeting() {Message = req.Query["name"]});
+        }
+
+        [SwaggerIgnore]
+        [FunctionName("swagger")]
+        public static async Task<IActionResult> Swagger(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger")]
+            HttpRequest req,
+            ILogger log)
+        {
+            var settings = new AzureFunctionsV2ToSwaggerGeneratorSettings()
+            {
+                Title = "My Function App Swagger",
+                Description = "Here be dragons!",
+                Version = "1.0"
+            };
+            var generator = new AzureFunctionsV2ToSwaggerGenerator(settings);
+            var swaggerDoc = await generator.GenerateForAzureFunctionClassAsync(typeof(ExampleFunctions));
+            var json = swaggerDoc.ToJson();
+            return new OkObjectResult(json);
+        }
+    }
+}
